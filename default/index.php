627d14e088688392320f4cfe65fec6aefb6d668e
Diego <anlumull@gmail.com>
2022-05-21 16:19:30 -0600

    primer commit

---
+++ default/index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="theme.css" type="text/css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    <div class="container justify-content-center"> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar9">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse text-center justify-content-center" id="navbar9">
+        <ul class="navbar-nav">
+          <li class="nav-item mx-2"> <a class="nav-link" href="#"></a> </li>
+          <li class="nav-item mx-2"> <a class="nav-link" href="#"></a> </li>
+          <li class="nav-item mx-2"> <a class="nav-link navbar-brand mr-0 text-white" href="#"><i class="fa d-inline fa-lg fa-stop-circle-o"></i>
+              <b> RECYWEB</b></a> </li>
+          <li class="nav-item mx-2"> <a class="nav-link" href="#"></a> </li>
+          <li class="nav-item mx-2"> <a class="nav-link" href="#"></a> </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="py-5" >
+    <div class="container">
+      <div class="row">
+        <div class="p-0 col-lg-5 order-2 order-lg-1"> <img class="img-fluid d-block" src="..\img\arbol1.webp"> </div>
+        <div class="d-flex flex-column justify-content-center p-3 col-lg-7 order-1 order-lg-2" >
+          <h1>Objetivo</h1>
+          <p class="lead mb-0">Nuestro objetivo es dar a conocer los centros de reciclajes en Quetzaltenango, para fomentar el habito de reciclaje, por este medio poder contactar con cada una de ellas sin correr algún riesgo, teniendo información puntual sobre la ubicación donde se encuentran, números de teléfono, redes sociales y también si hacen recolectas a domicilio.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <center><h4>Conoce las clasificaciones de desechos y lugares de centros de reciclaje</h4></center>
+  <div class="py-5 text-center" style="background: linear-gradient(-45deg, #96c42d , #bbe034,#dbf580, white,white) ;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-8 order-2 order-lg-1"> <img class="img-fluid d-block" src="..\img\caja1.png"> </div>
+        <div class="col-lg-2 col-4 d-flex flex-column justify-content-between order-3 order-lg-2"> <img class="img-fluid d-block" src="..\img\caja2.png" height="100"> <img class="img-fluid d-block" src="..\img\caja3.png"> </div>
+        <div class="px-md-5 p-3 d-flex flex-column justify-content-center col-lg-6 order-1 order-lg-3">
+          <i class="d-block fa fa-circle fa-3x mb-2 text-black-50"></i>
+          <h1 class="">Papel y cartón</h1>
+          <div class="row">
+            <div class="col-md-12"><a class="btn btn-outline-dark" href="papel.html">Ver</a> <a class="btn btn-outline-dark" href="ubipapel.html">Centros de reciclaje</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-8 order-2 order-lg-1"> <img class="img-fluid d-block" src="..\img\vidrio2.png"> </div>
+        <div class="col-lg-2 col-4 d-flex flex-column justify-content-between order-3 order-lg-2"> <img class="img-fluid d-block" src="..\img\vidrio3.png" height="100"> <img class="img-fluid d-block" src="..\img\vidrio1.png"> </div>
+        <div class="px-md-5 p-3 d-flex flex-column justify-content-center col-lg-6 order-1 order-lg-3">
+          <i class="d-block fa fa-circle fa-3x mb-2 text-primary"></i>
+          <h1 class="">Vidrio</h1>
+          <div class="row">
+            <div class="col-md-12"><a class="btn btn-outline-primary" href="vidrio.html">Ver</a> <a class="btn btn-outline-primary" href="ubividrio.html">Centros de reciclaje</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 text-center" style="background: linear-gradient(-45deg, #96c42d , #bbe034,#dbf580, white,white) ;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-8 order-2 order-lg-1"> <img class="img-fluid d-block" src="..\img\lata1.png"> </div>
+        <div class="col-lg-2 col-4 d-flex flex-column justify-content-between order-3 order-lg-2"> <img class="img-fluid d-block" src="..\img\lata2.png" height="100"> <img class="img-fluid d-block" src="..\img\lata3.png"> </div>
+        <div class="px-md-5 p-3 d-flex flex-column justify-content-center col-lg-6 order-1 order-lg-3">
+          <i class="d-block fa fa-circle fa-3x mb-2 text-black-50"></i>
+          <h1 class="">Plástico y latas</h1>
+          <div class="row">
+            <div class="col-md-12"><a class="btn btn-outline-dark" href="plastico.html">Ver</a> <a class="btn btn-outline-dark" href="ubilatas.html">Centros de reciclaje</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer>
+  <div class="py-1 " >
+    <div class="container" >
+      <div class="row" >
+        <div class="col-lg-2 col-6 p-3" > <i class="d-block fa fa-circle-o fa-5x text-primary"></i> </div>
+        <div class="col-lg-4 col-6 p-3" >
+          <h6>Comunicate con nosotros:</h6>
+          <p class="mb-0">Si quieres ser parte de este cambio, puedes comunicarte con nosotros, te dejamos los numeros de telefono y correo electronico para que nos escribas y podamos evaluar tu centro de reciclaje.&nbsp;</p>
+        </div>
+        <div class="col-md-4 p-3 " >
+          <p>Tel: 5921-9068<br>Tel: 5502-4196<br>Tel: 3019-2161</p>
+          <p class="mb-0">Correo: godinezsales21@gmail.com</p>
+         
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <p class="mb-0 mt-2">© 2022-2023&nbsp;</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+
+</html>
